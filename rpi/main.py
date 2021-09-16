b3f361bcba6cbdb917129a231ca07d61f252d570
KubaBoi <jakubanderle@outlook.cz>
2021-09-16 18:26:36 +0200

    debug

---
+++ rpi/main.py
@@ -0,0 +1,25 @@
+import time
+from adafruit_servokit import ServoKit
+
+from servo import Servo
+
+servoCount = 16
+
+pca = ServoKit(channels=servoCount)
+servos = []
+
+#init servos
+for i in range(servoCount):
+    pca.servo[i].set_pulse_width_range(500, 2500)
+    
+    servos.append(Servo(pca.servo[i], i))
+    servos[i].setAngle(0)
+
+servos[0].setMove(0.1, 180)
+
+#run
+while True:
+    for servo in servos:
+        servo.tick()
+
+    time.sleep(0.01)
