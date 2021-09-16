bd1dc0d9c0f1c65a7c5b042fde838c20ea880b1f
KubaBoi <jakubanderle@outlook.cz>
2021-09-16 20:46:22 +0200

    debug

--- rpi/server.py
+++ rpi/server.py
@@ -28,51 +28,52 @@ class Server:
         print("Running...")
 
     def serverThread(self, s):
-        try:
-            conn, addr = s.accept()
-            self.print_lock.acquire()
-            print("Connected by", addr)
-            i = 0
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    self.print_lock.release()
-                    break
+        while True:
+            try:
+                conn, addr = s.accept()
+                self.print_lock.acquire()
+                print("Connected by", addr)
+                i = 0
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        self.print_lock.release()
+                        break
 
-                decoded = data.decode("utf-8")
-                requests = json.loads(decoded) # array of recieved requests
+                    decoded = data.decode("utf-8")
+                    requests = json.loads(decoded) # array of recieved requests
 
-                answer = [] # answer in case of "g"
-                
-                for m in requests:
-                    type = m["type"]
-                    index = m["servo"]
-                    speed = m["speed"]
-                    angle = m["finalAngle"]
+                    answer = [] # answer in case of "g"
                     
-                    if (type == "m"): # set move
-                        self.controller.setMove(index, speed, angle)
-                    elif (type == "s"): # set angle
-                        self.controller.setAngle(index, angle)
-                    elif (type == "g"): # get angle
-                        get = {}
-                        get["servo"] = index
-                        get["angle"] = self.controller.getAngle(index)
-                        answer.append(get)
+                    for m in requests:
+                        type = m["type"]
+                        index = m["servo"]
+                        speed = m["speed"]
+                        angle = m["finalAngle"]
+                        
+                        if (type == "m"): # set move
+                            self.controller.setMove(index, speed, angle)
+                        elif (type == "s"): # set angle
+                            self.controller.setAngle(index, angle)
+                        elif (type == "g"): # get angle
+                            get = {}
+                            get["servo"] = index
+                            get["angle"] = self.controller.getAngle(index)
+                            answer.append(get)
 
-                
-                conn.sendall(bytes(json.dumps(answer), "utf-8"))
-            conn.close()
+                    
+                    conn.sendall(bytes(json.dumps(answer), "utf-8"))
+                conn.close()
 
-        except Exception as e:
-            print("Disconnected by", addr)
-            print(str(i) + ": ")
-            print(str(e))
-            i += 1
+            except Exception as e:
+                print("Disconnected by", addr)
+                print(str(i) + ": ")
+                print(str(e))
+                i += 1
 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.bind((self.HOST, self.PORT))
-            s.listen(5)
-            conn, addr = s.accept()
-            print("Connected by", addr)
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.bind((self.HOST, self.PORT))
+                s.listen(5)
+                conn, addr = s.accept()
+                print("Connected by", addr)
 
